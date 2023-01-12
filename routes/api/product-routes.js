1820489030173ff1ab1200e6c77d5bbb21723b2c
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-01-12 11:15:30 -0500

    added routes for all files in /routes/api

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category, where: {product_id: Product.id}}, {model: Tag, through: ProductTag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category, through: ProductTag}, {model: Tag, through: ProductTag}]
+    });
+
+    if (!productData) {
+      res.status(404).json({message: 'No product with this id'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
